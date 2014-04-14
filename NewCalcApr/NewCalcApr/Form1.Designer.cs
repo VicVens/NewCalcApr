367360b1b921eff954b3ac7a0323bb0bbb70dc51
KeewayRK <vicvens@yandex.ru>
2014-04-14 12:28:30 +0700

    added Addition, Subtraction operation

--- NewCalcApr/NewCalcApr/Form1.Designer.cs
+++ NewCalcApr/NewCalcApr/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.SecondInput = new System.Windows.Forms.TextBox();
+            this.FirsInput = new System.Windows.Forms.TextBox();
+            this.Addition = new System.Windows.Forms.Button();
+            this.Subtraction = new System.Windows.Forms.Button();
+            this.Multiplication = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(23, 26);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(267, 20);
+            this.Output.TabIndex = 0;
+            // 
+            // SecondInput
+            // 
+            this.SecondInput.Location = new System.Drawing.Point(195, 120);
+            this.SecondInput.Name = "SecondInput";
+            this.SecondInput.Size = new System.Drawing.Size(95, 20);
+            this.SecondInput.TabIndex = 1;
+            // 
+            // FirsInput
+            // 
+            this.FirsInput.Location = new System.Drawing.Point(23, 120);
+            this.FirsInput.Name = "FirsInput";
+            this.FirsInput.Size = new System.Drawing.Size(94, 20);
+            this.FirsInput.TabIndex = 2;
+            // 
+            // Addition
+            // 
+            this.Addition.Location = new System.Drawing.Point(23, 162);
+            this.Addition.Name = "Addition";
+            this.Addition.Size = new System.Drawing.Size(52, 42);
+            this.Addition.TabIndex = 3;
+            this.Addition.Text = "+";
+            this.Addition.UseVisualStyleBackColor = true;
+            this.Addition.Click += new System.EventHandler(this.addition_Click);
+            // 
+            // Subtraction
+            // 
+            this.Subtraction.Location = new System.Drawing.Point(92, 162);
+            this.Subtraction.Name = "Subtraction";
+            this.Subtraction.Size = new System.Drawing.Size(52, 42);
+            this.Subtraction.TabIndex = 4;
+            this.Subtraction.Text = "-";
+            this.Subtraction.UseVisualStyleBackColor = true;
+            this.Subtraction.Click += new System.EventHandler(this.Subtraction_Click);
+            // 
+            // Multiplication
+            // 
+            this.Multiplication.Location = new System.Drawing.Point(164, 162);
+            this.Multiplication.Name = "Multiplication";
+            this.Multiplication.Size = new System.Drawing.Size(52, 42);
+            this.Multiplication.TabIndex = 5;
+            this.Multiplication.Text = "*";
+            this.Multiplication.UseVisualStyleBackColor = true;
+            this.Multiplication.Click += new System.EventHandler(this.Multiplication_Click);
+            // 
+            // Division
+            // 
+            this.Division.Location = new System.Drawing.Point(238, 162);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(52, 42);
+            this.Division.TabIndex = 6;
+            this.Division.Text = "/";
+            this.Division.UseVisualStyleBackColor = true;
+            this.Division.Click += new System.EventHandler(this.Division_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 236);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.Multiplication);
+            this.Controls.Add(this.Subtraction);
+            this.Controls.Add(this.Addition);
+            this.Controls.Add(this.FirsInput);
+            this.Controls.Add(this.SecondInput);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.TextBox SecondInput;
+        private System.Windows.Forms.TextBox FirsInput;
+        private System.Windows.Forms.Button Addition;
+        private System.Windows.Forms.Button Subtraction;
+        private System.Windows.Forms.Button Multiplication;
+        private System.Windows.Forms.Button Division;
     }
 }
 
--- NewCalcApr/NewCalcApr/Form1.cs
+++ NewCalcApr/NewCalcApr/Form1.cs
@@ -16,5 +16,33 @@ namespace NewCalcApr
         {
             InitializeComponent();
         }
+
+        private void addition_Click(object sender, EventArgs e)
+        {
+            int first = Convert.ToInt32(FirsInput.Text);
+            int second = Convert.ToInt32(SecondInput.Text);
+            Output.Text = (first + second).ToString();
+        }
+
+        private void Subtraction_Click(object sender, EventArgs e)
+        {
+            int first = Convert.ToInt32(FirsInput.Text);
+            int second = Convert.ToInt32(SecondInput.Text);
+            Output.Text = (first - second).ToString();
+        }
+
+        private void Multiplication_Click(object sender, EventArgs e)
+        {
+            int first = Convert.ToInt32(FirsInput.Text);
+            int second = Convert.ToInt32(SecondInput.Text);
+            Output.Text = (first * second).ToString();
+        }
+
+        private void Division_Click(object sender, EventArgs e)
+        {
+            int first = Convert.ToInt32(FirsInput.Text);
+            int second = Convert.ToInt32(SecondInput.Text);
+            Output.Text = (first / second).ToString();
+        }
     }
 }
